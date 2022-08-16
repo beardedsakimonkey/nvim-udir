a496bf5066568a71c25e072a444aedb65b850119
beardedsakimonkey <sakimonkey@protonmail.com>
2022-08-15 21:49:16 -0700

    avoid nested udir buffers

--- lua/udir.lua
+++ lua/udir.lua
@@ -265,7 +265,7 @@ local function _35_()
   return false
 end
 M["config"] = {keymaps = {q = M.quit, h = M.up_dir, ["-"] = M.up_dir, l = M.open, ["<CR>"] = M.open, s = _32_, v = _33_, t = _34_, R = M.reload, d = M.delete, ["+"] = M.create, m = M.move, c = M.copy, ["."] = M.toggle_hidden_files}, show_hidden_files = true, is_file_hidden = _35_, sort = sort_by_name}
-M.udir = function(dir, _3ffrom_au)
+local function init(dir, _3ffrom_au)
   _G.assert((nil ~= dir), "Missing argument dir on lua/udir.fnl:206")
   local has_altbuf = (0 ~= vim.fn.bufexists(0))
   local origin_buf
@@ -308,5 +308,38 @@ M.udir = function(dir, _3ffrom_au)
   store["set!"](buf, state)
   render(state)
   return u["set-cursor-pos"](_3forigin_filename)
+end
+local function update_instance(dir)
+  _G.assert((nil ~= dir), "Missing argument dir on lua/udir.fnl:233")
+  local state = store.get(vim.fn.bufnr("#"))
+  local cwd
+  if ("" ~= dir) then
+    cwd = fs.realpath(vim.fn.expand(dir))
+  else
+    local p = vim.fn.expand("%:p:h")
+    if ("" ~= p) then
+      cwd = fs.realpath(p)
+    else
+      cwd = assert(vim.loop.cwd())
+    end
+  end
+  vim.cmd("noau bd")
+  update_cwd(state, cwd)
+  render(state)
+  return u["update-buf-name"](state.buf, state.cwd)
+end
+M.udir = function(dir, _3ffrom_au)
+  _G.assert((nil ~= dir), "Missing argument dir on lua/udir.fnl:244")
+  local is_altbuf_udir
+  if _3ffrom_au then
+    is_altbuf_udir = vim.fn.getbufvar(vim.fn.bufnr("#"), "is_udir", false)
+  else
+    is_altbuf_udir = false
+  end
+  if is_altbuf_udir then
+    return update_instance(dir)
+  else
+    return init(dir, _3ffrom_au)
+  end
 end
 return M
